1fec22c20c7ff029f81d8506846c566716530ea8
LeoRiether <leonardoalves8217@gmail.com>
2022-07-03 21:27:59 -0300

    Oh god I almost deleted this file
    this is not a commit, it's a backup

--- base64.cpp
+++ base64.cpp
@@ -1,24 +1,25 @@
-// #pragma once
+#pragma once
 
+#include "types.h"
 #include <iostream>
 #include <string>
 #include <cassert>
 using std::string;
 
 namespace base64 {
-    char encoding[64];
-    char inv_encoding[256];
+    byte_t encoding[64];
+    byte_t inv_encoding[256];
     bool built = false;
     void ensure_encoding_built() {
         if (built) return;
         built = true;
 
         int p = 0;
-        for (char c = 'A'; c <= 'Z'; c++) // 26
+        for (byte_t c = 'A'; c <= 'Z'; c++) // 26
             encoding[p++] = c;
-        for (char c = 'a'; c <= 'z'; c++) // 26
+        for (byte_t c = 'a'; c <= 'z'; c++) // 26
             encoding[p++] = c;
-        for (char c = '0'; c <= '9'; c++) // 10
+        for (byte_t c = '0'; c <= '9'; c++) // 10
             encoding[p++] = c;
         encoding[p++] = '+'; // 1
         encoding[p++] = '/'; // 1
@@ -27,7 +28,7 @@ namespace base64 {
             inv_encoding[(int)encoding[i]] = i;
     }
 
-    string encode(const char* buf, const int n) {
+    string encode(const byte_t* buf, const int n) {
         ensure_encoding_built();
         string e;
         int pad = (3 - n%3)%3;
@@ -53,7 +54,7 @@ namespace base64 {
     }
 
     // Decode `s` and put the output into `out`
-    int decode(const string& s, char* out) {
+    int decode(const string& s, byte_t* out) {
         int p = 0;
         int n = s.size();
         for (int i = 0; i < n; i += 4) {
@@ -61,23 +62,13 @@ namespace base64 {
             auto at = [&](int j) { return inv_encoding[(int)s[j]]; };
             out[p++] = (at(i) << 2) | (at(i+1) >> 4);
 
-            if (s[i+1] != '=')
+            if (s[i+2] != '=')
                 out[p++] = ((at(i+1) & 15) << 4) | (at(i+2) >> 2);
 
-            if (s[i+2] != '=')
+            if (s[i+3] != '=')
                 out[p++] = ((at(i+2) & 3) << 6) | at(i+3);
         }
         out[p] = 0;
         return p;
     }
-}
-
-int main() {
-    string a; getline(std::cin, a);
-    auto e = base64::encode(a.c_str(), a.size());
-    std::cout << e << std::endl;
-
-    char out[a.size()+1];
-    base64::decode(e, out);
-    std::cout << out << std::endl;
 }
